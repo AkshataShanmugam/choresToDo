7c30e0e2f2cf3edb06dd263e744caea975d01881
AkshataShanmugam <Akshata.Shanmugam@gmail.com>
2024-08-20 21:34:40 +0530

    Hiding envs

--- src/firebase.jsx
+++ src/firebase.jsx
@@ -6,13 +6,7 @@ import { getDatabase } from 'firebase/database';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAktdYPOVydL3Vz1op1wBFGKItuPNjN4Jw",
-  authDomain: "chores-todo-70e4f.firebaseapp.com",
-  projectId: "chores-todo-70e4f",
-  storageBucket: "chores-todo-70e4f.appspot.com",
-  messagingSenderId: "754988393119",
-  appId: "1:754988393119:web:99626658b2dafb7c5e38aa",
-  databaseURL: "https://chores-todo-70e4f-default-rtdb.asia-southeast1.firebasedatabase.app/"
+
 };
 
 // Initialize Firebase
