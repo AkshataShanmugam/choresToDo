9372eac6ac13ea5c4c95496803b891bc6ad09353
AkshataShanmugam <Akshata.Shanmugam@gmail.com>
2024-08-22 10:02:09 +0530

    Resolving errors related to env vars

--- src/firebase.jsx
+++ src/firebase.jsx
@@ -1,16 +1,14 @@
 import { initializeApp } from "firebase/app";
 import { getDatabase } from 'firebase/database';
-// import 'dotenv/config';
 
-// console.log(process.env.databaseURL)
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "1",
-  authDomain: "1",
-  projectId: "1",
-  messagingSenderId: "1",
-  appId: "1",
-  databaseURL: "1",
+  apiKey: import.meta.env.VITE_FIREBASE_API,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  databaseURL: import.meta.env.VITE_FIREBASE_DATABASE_URL,
 };
 
 
