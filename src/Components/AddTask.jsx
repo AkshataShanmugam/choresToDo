346b14f7ee1c8c95a89c28e7501bfd749d52e8a7
AkshataShanmugam <Akshata.Shanmugam@gmail.com>
2024-08-22 11:27:56 +0530

    Made minor UI changes to make it better for mobile usage, added a file to keep track of features or things to do in this

--- src/Components/AddTask.jsx
+++ src/Components/AddTask.jsx
@@ -33,7 +33,9 @@ function AddTask() {
                 placeholder="Enter time to do (minutes)"
                 style={{ marginRight: '10px', padding: '5px' }}
             />
-            <Button onClick={handleAddTask} variant="outlined" style={{ marginRight: '30px' }}>
+            <br></br>
+            <br></br>
+            <Button onClick={handleAddTask} variant="outlined">
                 Add Task
             </Button>
         </div>
