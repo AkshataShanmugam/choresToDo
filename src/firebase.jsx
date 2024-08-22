e4d632b53446b98df76ad215a1f015569e18d30d
AkshataShanmugam <Akshata.Shanmugam@gmail.com>
2024-08-22 09:55:26 +0530

    Made changes in gitignore file

--- src/firebase.jsx
+++ src/firebase.jsx
@@ -1,21 +1,18 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 import { getDatabase } from 'firebase/database';
-import 'dotenv/config';
+// import 'dotenv/config';
 
+// console.log(process.env.databaseURL)
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: process.env.VITE_API,
-  authDomain: process.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.VITE_FIREBASE_PROJECT_ID,
-  messagingSenderId: process.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.VITE_FIREBASE_APP_ID,
-  databaseURL: process.env.VITE_FIREBASE_DATABASE_URL
+  apiKey: "1",
+  authDomain: "1",
+  projectId: "1",
+  messagingSenderId: "1",
+  appId: "1",
+  databaseURL: "1",
 };
 
-console.log(databaseURL)
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
